d469c2da48442998e0cd5df526c2607d916784c3
DiegoJavier <dijaq08@gmail.com>
2018-12-16 15:29:55 -0500

    Update Entity recognition week 10

--- main.cpp
+++ main.cpp
@@ -7,25 +7,41 @@ using namespace std;
 int main()
 {
 	string line;
-	ifstream f("train.txt");
-	ofstream write("_train.txt");
+	ifstream f("testb.txt");
+	ofstream write("_testb.csv");
 	if (f.is_open())
 	{
 		while ( getline (f,line) )
 		{
 			//cout << line << '\n';
 			int j=0;
+			string linea = "";
 			for(int i=0; i<line.length(); i++)
 			{
 				if((line.substr(i,1) == " ") || (i == (line.length()-1)))
 				{
-					//write << cout << i << endl;
-					write << line.substr(j,i);
+					string imp="";
+					for(int k=0; k<line.substr(j,i).length(); k++)
+					{
+						if((line.substr(j+k,1) == ",")  || (line.substr(j,i) == "\"") || (line.substr(j,i) == ";"))
+						{
+							imp += "-";
+						}
+						else
+						{
+							imp += line.substr(j+k,1);
+						}
+					}
+					linea+=imp+",";
+					//write << line.substr(j,i);
 					j = i;
 				//cout << line.substr(i,1) << '\n';
 				}
 			}
-			write << endl;
+			//write << "\n";
+			linea += "\n";
+			write << linea;
+			//write << endl;
 		}
 		f.close();
 	}
